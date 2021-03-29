61c3b7d00b12d14fe7533a0ccbd23673f10ba4ac
costalferz <48489419+costalferz@users.noreply.github.com>
2021-03-29 21:27:55 +0700

    Delete week06 directory

--- week06/myapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic.detail import DetailView
-from django.shortcuts import render,redirect
-from myapp.models import Car
-from django.http import HttpResponse
-
-
-class CarDetailView(DetailView):
-    model = Car
-    template_name = 'car.html'
