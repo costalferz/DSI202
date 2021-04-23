32499e22450316f4a870e135107105da8c9de82e
costalferz <48489419+costalferz@users.noreply.github.com>
2021-04-23 19:11:45 +0700

    Delete finalProj/elon directory

--- finalProj/elon/item/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path
-#from .views import *
-from item import views
-urlpatterns = [
-    path('',views.home,name='home')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
